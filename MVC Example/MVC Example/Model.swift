478c5802270bd7f643201a3a0dd5633fa6c5e7a6
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-10 14:51:52 +0300

    add model

---
+++ MVC Example/MVC Example/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  MVC Example
+//
+//  Created by Арсентий Халимовский on 10.05.2023.
+//
+
+import Foundation
+
+// Model
+struct Person {
+    let name: String
+    let surname: String
+}
